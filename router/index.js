86f2a10b38029563fe597619bdd49cc62ee31ee3
perassi <horacio2jp@gmail.com>
2022-08-08 18:10:44 +0300

    Update index.js

--- router/index.js
+++ router/index.js
@@ -1,7 +1,15 @@
-const Router = require('express').Router;
-const userController = require('../controllers/user.controller');
+const Router = require("express").Router;
+const axios = require("axios");
+
+const userController = require("../controllers/user.controller");
 const router = new Router();
 
-router.get('/', (req, res) => {res.json('OK')});
+router.get("/", async (req, res) => {
+  const response = await axios.get("http://178.20.43.144:5000/");
+
+  console.log(response.data);
+
+  res.json(response.data);
+});
 
 module.exports = router;
